b7203a29983c0e742f45c10dcb2342437381f0e0
MateoNerli <mnerli2003@gmail.com>
2024-01-19 23:15:36 -0300

    Se realizo la skill

---
+++ src/utils/skills.js
@@ -0,0 +1,63 @@
+import html from "../assets/Skills/HTML5.png";
+import css from "../assets/Skills/CSS3.png";
+import js from "../assets/Skills/JavaScript.png";
+import react from "../assets/Skills/React.png";
+import vue from "../assets/Skills/Vue.png";
+import CSharp from "../assets/Skills/C-Sharp.png";
+import git from "../assets/Skills/Git.png";
+import github from "../assets/Skills/GitHub.png";
+import mySql from "../assets/Skills/MySQL.png";
+import node from "../assets/Skills/nodejs.png";
+import angular from "../assets/Skills/angular.png";
+import python from "../assets/Skills/python.png";
+
+export const SkillsData = [
+  {
+    name: "HTML",
+    icon: html,
+  },
+  {
+    name: "CSS",
+    icon: css,
+  },
+  {
+    name: "JavaScript",
+    icon: js,
+  },
+  {
+    name: "React",
+    icon: react,
+  },
+  {
+    name: "Vue",
+    icon: vue,
+  },
+  {
+    name: "C#",
+    icon: CSharp,
+  },
+  {
+    name: "Git",
+    icon: git,
+  },
+  {
+    name: "GitHub",
+    icon: github,
+  },
+  {
+    name: "MySQL",
+    icon: mySql,
+  },
+  {
+    name: "Node",
+    icon: node,
+  },
+  {
+    name: "Angular",
+    icon: angular,
+  },
+  {
+    name: "Python",
+    icon: python,
+  },
+];
