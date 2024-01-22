0ad43392d53435bfc5f143bad98e2457b453ed6a
MateoNerli <mnerli2003@gmail.com>
2024-01-22 13:21:19 -0300

    cambios

--- src/components/sobreMi.jsx
+++ src/components/sobreMi.jsx
@@ -15,21 +15,6 @@ export const SobreMi = () => {
     <>
       <div className="overflow-hidden bg-[#27292a] m-[auto] p-[10px] p-[15px] text-white flex justify-center">
         <div className="max-w-[1200px] flex justify-center algin-center flex-wrap">
-          <div className="hidden">
-            <div>
-              <div className="text-center mb-[20px]" id="sobre-mi">
-                <div className="text-3xl font-Righteous mb-[25px] text-center text-white relative border-b-2">
-                  <p className="text-3xl font-sans mb-[25px] text-center text-white p-[1rem] relative">
-                    Sobre Mi
-                  </p>
-                </div>
-                <h2 className="text-3xl font-bold m-[20px] m-[0] tracking-wider">
-                  Hola, Soy <span className="text-[#66c7ff]">Mateo Nerli</span>
-                </h2>
-                <h3 className="text-lg">Programador || Jr.Dev</h3>
-              </div>
-            </div>
-          </div>
           <div
             data-aos="flip-left"
             className="p-[30px] max-w-[400px] text-center"
@@ -49,7 +34,7 @@ export const SobreMi = () => {
                 Hola, Soy <span className="text-[#66c7ff]">Mateo Nerli</span>
               </h2>
               <h3 className="text-lg font-medium uppercase mb-[20px] tracking-wider">
-                Futuro programador
+                Programador || Jr.Dev
               </h3>
             </div>
 
